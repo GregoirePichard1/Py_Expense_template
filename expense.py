52afdde15ebb71846ca4ecd51b3116a06e0275c2
Gregoire Pichard <gregoire.peichard@epita.fr>
2022-11-02 21:42:51 +0100

    first part of the todo list

--- expense.py
+++ expense.py
@@ -1,4 +1,7 @@
 from PyInquirer import prompt
+from csv import writer
+from user import *
+
 
 expense_questions = [
     {
@@ -16,15 +19,52 @@ expense_questions = [
         "name":"spender",
         "message":"New Expense - Spender: ",
     },
-
+    {
+        "type":"input",
+        "name":"involved",
+        "message":"New Expense - Involved: ",
+    },
 ]
 
 
-
 def new_expense(*args):
+    getListUser()
     infos = prompt(expense_questions)
-    # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
+    if not checkExistingUser(infos["spender"]):
+        print("Please enter a valid spender")
+        new_expense(*args)
+    with open('expense_report.csv', 'a', newline='') as f_object:  
+        writer_object = writer(f_object)
+        l = []
+        l.append(infos["amount"])
+        l.append(infos["label"])
+        l.append(infos["spender"])
+        l.append(infos["involved"])
+        writer_object.writerow(l)
+        f_object.close()
     print("Expense Added !")
     return True
 
+def checkExistingUser(name):
+    with open("users.csv") as fp:
+        line = fp.readline()
+        cnt = 1
+        while line:
+            if line.rstrip() == name:
+                return True
+            line = fp.readline()
+            cnt += 1
+        return False
+    return False
 
+def getListUser():
+    with open("users.csv") as fp:
+        line = fp.readline()
+        cnt = 1
+        l = []
+        print("Users: ", end=' ')
+        while line:
+            print(line.rstrip() + ", ", end= ' ')
+            line = fp.readline()
+            cnt += 1
+        print()
--- user.py
+++ user.py
@@ -1,8 +1,17 @@
 from PyInquirer import prompt
+from csv import writer
+
 user_questions = [
 
 ]
 
 def add_user():
-    # This function should create a new user, asking for its name
+    name = input("Enter your name: ")
+    if name != "" and name != "\n":
+        with open('users.csv', 'a', newline='') as f_object:  
+            writer_object = writer(f_object)
+            l = []
+            l.append(name)
+            writer_object.writerow(l)
+            f_object.close()
     return
